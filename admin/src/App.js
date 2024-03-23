7207440395cb8db216127f5d9bb56a988ebfd0fd
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-23 14:29:44 +0700

    W1-dashboard: Implement login page and it's css

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,18 +1,23 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import LoginForm  from "./components/loginForm/LoginForm";
 
 function App() {
   const [theme, colorMode] = useMode();
   
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <main className="content"></main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <div>
+      <LoginForm />
+
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <main className="content"></main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </div>
   );
 }
 
