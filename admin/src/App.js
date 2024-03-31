0626b67a2724d2b88c55e34ccca8d5dfe9f0d017
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-31 16:03:53 +0700

    W2-dashboard: Update invoices index page and create user page

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,9 +7,9 @@ import Sidebar from "./scenes/global/SideBar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -35,8 +35,8 @@ function App() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
                 <Route path="/contacts" element={<Contacts />} />
-                {/* <Route path="/invoices" element={<Invoices />} /> */}
-                {/* <Route path="/form" element={<Form />} /> */}
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
