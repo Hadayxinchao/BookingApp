7645dd32f69c573f000074751f44719f6c8afbf7
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-31 16:30:59 +0700

    W2-dashboard: Create Pie Chart

--- admin/src/App.js
+++ admin/src/App.js
@@ -11,7 +11,7 @@ import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
 import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar/calendar";
@@ -38,7 +38,7 @@ function App() {
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
                 <Route path="/bar" element={<Bar />} />
-                {/* <Route path="/pie" element={<Pie />} /> */}
+                <Route path="/pie" element={<Pie />} />
                 {/* <Route path="/line" element={<Line />} /> */}
                 <Route path="/faq" element={<FAQ />} />
                 <Route path="/calendar" element={<Calendar />} />
