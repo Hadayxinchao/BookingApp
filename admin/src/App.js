d1ebb2c7b7252210a473d1c525d5190cc6263d00
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-31 16:17:33 +0700

    W2-dashboard: Create faq page

--- admin/src/App.js
+++ admin/src/App.js
@@ -12,7 +12,7 @@ import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar/calendar";
 
@@ -40,7 +40,7 @@ function App() {
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
-                {/* <Route path="/faq" element={<FAQ />} /> */}
+                <Route path="/faq" element={<FAQ />} />
                 <Route path="/calendar" element={<Calendar />} />
                 {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
