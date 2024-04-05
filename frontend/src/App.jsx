bcc13acaad0a00502b1419df4415558004eb2b3f
LongK <hoanglong7802@gmail.com>
2024-04-05 22:45:08 +0700

    Fix css error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import IndexPage from "./components/Index";
 import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Route>
