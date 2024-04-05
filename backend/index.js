9e6c37139ecd5d99ef07276b2454d5aa45497e96
Hoang Ha <SmithMerch2301@gmail.com>
2024-04-05 22:32:48 +0700

    W3-dashboard: Update Backend configuration for Admin Dashboard

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
 const express = require('express');
 const cors = require('cors');
 const connectWithDB = require('./config/db');
-require('dotenv').config();
+dotenv.config();
 // const cloudinary = require('cloudinary').v2;
 
 // connect with database
@@ -16,8 +28,14 @@ connectWithDB();
 
 const app = express();
 
+/* CONFIGURATION */
 // middleware to handle json
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy( { policy: 'cross-origin' } ));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
   cors({
@@ -26,8 +44,14 @@ app.use(
   }),
 );
 
+/* ROUTES */
 // use express router
 app.use('/', require('./routes'));
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
+
 
 app.listen(process.env.PORT || 8001, (err) => {
   if (err) {
