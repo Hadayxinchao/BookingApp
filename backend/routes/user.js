4d5344174aea9871b2a0680128a2888e1d1c7d33
Hoang AnH <dohoanganh16032002@gmail.com>
2024-04-18 22:18:19 +0700

    Fix upload image - not done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 
 const {
   register,
   login,
-  logout,
   profile,
   updateProfile,
   uploadByLink,
@@ -16,8 +18,8 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/profile').get(profile);
 /* router.route('/updateProfile').post(updateProfile);
-router.route('/upload', upload.array('photos', 100)).post(uploadFromLocal);
 router.route('/upload-profile-picture/:id', upload.single('profilePicture')).post(profilePicture); */
 router.route('/upload-by-link').post(uploadByLink);
+router.route('/upload', upload.array('photos', 100)).post(uploadFromLocal);
 router.route('/update').post(updateProfile);
 module.exports = router;
