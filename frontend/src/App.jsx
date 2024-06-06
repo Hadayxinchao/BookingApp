266e41804a7ad6f0ec3d09cd146fbf210ab96f01
Hoang AnH <dohoanganh16032002@gmail.com>
2024-06-06 07:30:32 +0700

    Delete admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,25 +23,25 @@ axios.defaults.withCredentials = true;
 function App() {
   return (
     <div>
-        <PlaceProvider>
-      <UserProvider>
-        <ToastContainer />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/account" element={<ProfilePage />} />
-            <Route path="/account/places" element={<PlacesPage />} />
-            <Route path="/account/places/new" element={<PlacesFormPage />} />
-            <Route path="/account/places/:id" element={<PlacesFormPage />} />
-            <Route path="/account/bookings" element={<BookingsPage />} />
-            <Route path="/account/bookings/:id" element={<BookingPage />} />
-            <Route path="/place/:id" element={<PlacePage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </UserProvider>
+      <PlaceProvider>
+        <UserProvider>
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/account" element={<ProfilePage />} />
+              <Route path="/account/places" element={<PlacesPage />} />
+              <Route path="/account/places/new" element={<PlacesFormPage />} />
+              <Route path="/account/places/:id" element={<PlacesFormPage />} />
+              <Route path="/account/bookings" element={<BookingsPage />} />
+              <Route path="/account/bookings/:id" element={<BookingPage />} />
+              <Route path="/place/:id" element={<PlacePage />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </UserProvider>
       </PlaceProvider>
     </div>
   );
